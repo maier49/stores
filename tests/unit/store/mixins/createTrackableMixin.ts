76af7a4e003c20d8326680f6e074a98bf04156eb
Bradley Maier <bmaier@sitepen.com>
2016-10-22 00:35:06 -0400

    Make file names and exports more consistent
    Use `createX` pattern for all modules that export factory
    functions.
    
    Export factory function as default for all modules that
    export factory functions.
    
    Move query interface to interfaces file, to separate it
    from the createCompoundQuery factory.

--- tests/unit/store/mixins/createTrackableMixin.ts
+++ tests/unit/store/mixins/createTrackableMixin.ts
@@ -1,6 +1,7 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import Promise from 'dojo-shim/Promise';
+import { StoreOptions, CrudOptions, UpdateResults } from '../../../../src/store/createStore';
 import createTrackableMixin, {
 	TrackableOptions, TrackableMixin,
 	TrackedObservableStoreMixin
